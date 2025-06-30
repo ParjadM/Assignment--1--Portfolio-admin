9aeaf0229e3b55991bd900656bb69f84f40fa93a
Parjad Minooei <155120660+ParjadM@users.noreply.github.com>
2025-06-30 15:18:52 -0400

    Update index.js

--- index.js
+++ index.js
@@ -97,4 +97,5 @@ app.post('/edit/skill/:id', async (req, res) => {
   res.redirect('/');
 });
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.listen(port, () => console.log(`Server running on port ${port}`));
+
